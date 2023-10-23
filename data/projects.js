8641db488ed852766361ae2b931571d345560c43
Mohamed Abdelhakem <107258224+MohamedAbdelhakem17@users.noreply.github.com>
2023-10-23 22:17:14 +0300

    img fix

--- data/projects.js
+++ data/projects.js
@@ -6,43 +6,43 @@ export const projects = [
         projectName: "AQRA"
     },
     {
-        img: "../img/projects/DANIELS.png",
+        img: "https://mohamedabdelhakem17.github.io/portfolio/img/projects/DANIELS.png",
         link: "https://mohamedabdelhakem17.github.io/DANIELS/",
         githubLink: "https://github.com/MohamedAbdelhakem17/DANIELS",
         projectName: "DANIELS"
     },
     {
-        img: "../img/projects/theEvent.png",
+        img: "https://mohamedabdelhakem17.github.io/portfolio/img/projects/theEvent.png",
         link: "https://the-eventt.netlify.app/",
         githubLink: "https://github.com/MohamedAbdelhakem17/theEvent",
         projectName: "The Event"
     },
     {
-        img: "../img/projects/Medicio.png",
+        img: "https://mohamedabdelhakem17.github.io/portfolio/img/projects/Medicio.png",
         link: "https://mohamedabdelhakem17.github.io/Medicio-hospital/",
         githubLink: "https://github.com/MohamedAbdelhakem17/Medicio-hospital",
         projectName: "Medicio"
     },
     {
-        img: "../img/projects/gymx.png",
+        img: "https://mohamedabdelhakem17.github.io/portfolio/img/projects/gymx.png",
         link: "https://gymmxx.netlify.app",
         githubLink: "https://github.com/MohamedAbdelhakem17/gymx",
         projectName: "Gym-X"
     },
     {
-        img: "../img/projects/Yummy.png",
+        img: "https://mohamedabdelhakem17.github.io/portfolio/img/projects/Yummy.png",
         link: "https://menussssss.netlify.app/",
         githubLink: "https://github.com/MohamedAbdelhakem17/Yummy",
         projectName: "Yummy"
     },
     {
-        img: "../img/projects/Movie.png",
+        img: "https://mohamedabdelhakem17.github.io/portfolio/img/projects/Movie.png",
         link: "https://noooxetvvv.netlify.app/",
         githubLink: "null",
         projectName: "Movie App"
     },
     {
-        img: "../img/projects/Dolphin.png",
+        img: "https://mohamedabdelhakem17.github.io/portfolio/img/projects/Dolphin.png",
         link: "https://mohamedabdelhakem17.github.io/Dolphin-In-Biology/",
         githubLink: "https://github.com/MohamedAbdelhakem17/Dolphin-In-Biology",
         projectName: "Dolphin In Biology"
